12f077baea31321a27285fb4eed0669ab769734f
Stephen Reindl <stephen@stephenreindl.net>
2016-11-08 00:52:50 +0100

    fix compiler warnings

--- EF Intro/ConventionsExample/ConventionsExample/Program.cs
+++ EF Intro/ConventionsExample/ConventionsExample/Program.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConventionsExample
+﻿namespace ConventionsExample
 {
+    // ReSharper disable once ClassNeverInstantiated.Global
     class Program
     {
+        // ReSharper disable once ArrangeTypeMemberModifiers
+        // ReSharper disable once UnusedParameter.Local
         static void Main(string[] args)
         {
         }
