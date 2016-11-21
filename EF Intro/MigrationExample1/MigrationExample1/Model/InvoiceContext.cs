e72eed35a4d626a709f8887a52be7dd48357fce1
Stephen Reindl <stephen@stephenreindl.net>
2016-11-21 22:17:12 +0100

    Migration Example 2

---
+++ EF Intro/MigrationExample1/MigrationExample1/Model/InvoiceContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MigrationExample1.Model
+{
+    public class InvoiceContext: DbContext
+    {
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
---
+++ EF Intro/MigrationExample1/MigrationExample1/Model/ModelEntities.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MigrationExample1.Model
+{
+    public class Invoice
+    {
+        [Key]
+        public Guid InvoiceId { get; set; }
+
+        [Required, StringLength(24), Index(IsUnique = true)]
+        public string InvoiceNumber { get; set; }
+
+        public Customer Customer { get; set; }
+
+        public Collection<Item> Items { get; set; }
+    }
+
+    public class Item
+    {
+        [Key]
+        public Guid ItemId { get; set; }
+
+        public short Position { get; set; }
+
+        public string Text { get; set; }
+
+        public decimal Price { get; set; }
+    }
+
+    public class Customer
+    {
+        [Key]
+        public Guid CustomerId { get; set; }
+
+        [Required, StringLength(24), Index(IsUnique = true)]
+        public string CustomerNo { get; set; }
+
+        public string Name { get; set; }
+    }
+}
